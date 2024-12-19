3e97f5acc91535da3aff9d855072e2b3b2517dc3
JohnWho <152762626+bardan1@users.noreply.github.com>
2024-12-19 12:37:23 +0545

    uploaded

---
+++ courseWorkExpenseTrackingApp/courseWorkExpenseTrackingApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace courseWorkExpenseTrackingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
